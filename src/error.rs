f06e66052cddb29c1e2f6c1f8672308afb9bd99d
Peter Lyons <pete@peterlyons.com>
2018-08-25 15:41:36 -0600

    Live Coding run 2 done

---
+++ src/error.rs
@@ -0,0 +1,25 @@
+use std::{convert, fmt, num};
+
+pub struct DivideError {
+    message: String,
+}
+
+impl fmt::Debug for DivideError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl convert::From<num::ParseFloatError> for DivideError {
+    fn from(_error: num::ParseFloatError) -> Self {
+        DivideError {
+            message: "Please provide valid numbers only".into(),
+        }
+    }
+}
+
+pub fn exit(message: &str) -> Result<(), DivideError> {
+    Err(DivideError {
+        message: message.into(),
+    })
+}
