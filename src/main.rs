9f7bdd8c73666ef7e6c219bdf83f778d11a7010e
Peter Lyons <pete@peterlyons.com>
2018-08-26 17:41:09 -0600

    checkpointc

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-#[derive(Debug)]
+use std::fmt;
 struct DivideError {
     message: String,
+}
+impl fmt::Debug for DivideError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.message)
+    }
 }
 
 fn main() -> Result<(), DivideError> {
     println!("Hello, world!");
-    Err(DivideError { message: "Failed".into() })
+    Err(DivideError {
+        message: "Failed".into(),
+    })
 }
